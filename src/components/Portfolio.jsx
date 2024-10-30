e2d89e0071075da2881cc7413f5e711b293317b7
Ihor Panchenko <ipanchenko91@gmail.com>
2024-10-30 17:30:27 +0100

    Add Footer component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,6 +5,7 @@ import project3 from "../assets/project3.png";
 import project4 from "../assets/project4.png";
 import project5 from "../assets/project5.png";
 import project6 from "../assets/project6.png";
+import Reveal from "./Reveal";
 
 const projects = [
   {
@@ -61,44 +62,46 @@ const Portfolio = () => {
       <h2 className="text-3xl font-bold text-gray-200 mb-8">Portfolio</h2>
 
       {projects.map((project, index) => (
-        <div
-          key={index}
-          className={`flex flex-col md:flex-row ${
-            index % 2 !== 0 ? "md:flex-row-reverse" : ""
-          } mb-12`}
-        >
-          <div className="w-full md:w-1/2 p-4">
-            <img
-              src={project.img}
-              alt={project.title}
-              className="w-full h-full object-cover rounded-lg shadow-lg"
-            />
-          </div>
+        <Reveal>
+          <div
+            key={index}
+            className={`flex flex-col md:flex-row ${
+              index % 2 !== 0 ? "md:flex-row-reverse" : ""
+            } mb-12`}
+          >
+            <div className="w-full md:w-1/2 p-4">
+              <img
+                src={project.img}
+                alt={project.title}
+                className="w-full h-full object-cover rounded-lg shadow-lg"
+              />
+            </div>
 
-          <div className="w-full md:w-1/2 p-4 flex flex-col justify-center">
-            <h3 className="text-2xl font-semibold text-gray-200 mb-4">
-              {project.title}
-            </h3>
-            <p className="text-gray-300 mb-4">{project.description}</p>
+            <div className="w-full md:w-1/2 p-4 flex flex-col justify-center">
+              <h3 className="text-2xl font-semibold text-gray-200 mb-4">
+                {project.title}
+              </h3>
+              <p className="text-gray-300 mb-4">{project.description}</p>
 
-            <div className="flex space-x-4">
-              <a
-                href={project.links.site}
-                className="px-4 py-2 bg-slate-600 text-gray-200 rounded-lg 
+              <div className="flex space-x-4">
+                <a
+                  href={project.links.site}
+                  className="px-4 py-2 bg-slate-600 text-gray-200 rounded-lg 
                 hover:bg-slate-700 transition duration-300"
-              >
-                View Site
-              </a>
-              <a
-                href={project.links.github}
-                className="px-4 py-3 bg-slate-600 text-gray-200 rounded-lg 
+                >
+                  View Site
+                </a>
+                <a
+                  href={project.links.github}
+                  className="px-4 py-3 bg-slate-600 text-gray-200 rounded-lg 
               hover:bg-slate-700 transition duration-300"
-              >
-                <AiOutlineGithub />
-              </a>
+                >
+                  <AiOutlineGithub />
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        </Reveal>
       ))}
     </div>
   );
